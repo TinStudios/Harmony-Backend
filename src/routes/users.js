fe85c636a582846aaef8f82e0c553c3299884675
SapphireDisD <villagerlover52@hotmail.com>
2021-10-09 17:28:44 -0300

    Automatic commit

--- src/routes/users.js
+++ src/routes/users.js
@@ -8,13 +8,18 @@ module.exports = (websockets, app, database, checkLogin) => {
                     if (!err) {
                         const user = dbRes.rows.find(x => x.token == req.headers.authorization);
                         const discriminator = dbRes.rows.find(x => x.username == req.body.username && x.discriminator == user.discriminator) ? generateDiscriminator(dbRes.rows.filter(x => x.username == req.body.username)) : user.discriminator;
-                        database.query(`UPDATE users SET username = $1, discriminator = $2, password = $3 WHERE id = '${userId}'`, [req.body.username ?? user.username, discriminator, await argon2.hash(req.body.password, { type: argon2.argon2id }) ?? user.password], err => {
+                        const token = req.body.password ? "Bearer " +  await generateToken({ id: user.id }) : user.token;
+                        database.query(`UPDATE users SET username = $1, discriminator = $2, password = $3, token = $4 WHERE id = '${userId}'`, [req.body.username ?? user.username, discriminator, await argon2.hash(req.body.password, { type: argon2.argon2id }) ?? user.password, token], err => {
                             if (!err) {
                                 const returnedUser = Object.keys(user).reduce((obj, key, index) => key != "token" && key != "password" ? ({ ...obj, [key]: Object.keys(user).map(x => x == "guilds" ? JSON.parse(user[x]) : user[x])[index] }) : null, {});
                                 returnedUser.username = req.body.username;
                                 returnedUser.discriminator = discriminator;
                                 websockets[req.headers.authorization]?.forEach(websocket => {
                                     websocket.send(JSON.stringify({ event: "userChange", id: userId, username: req.body.username }));
+                                    if(req.body.password) {
+                                        websocket.send(JSON.stringify({ event: "tokenChange", newToken: token }));
+                                        websocket.terminate();
+                                    }
                                 });
                                 res.send(returnedUser);
                             } else {
