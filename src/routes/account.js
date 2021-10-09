bfbef6b320f7e5912bffb0b639f1ee60faaa75d5
SapphireDisD <villagerlover52@hotmail.com>
2021-10-09 17:15:50 -0300

    Automatic commit

--- src/routes/account.js
+++ src/routes/account.js
@@ -19,6 +19,8 @@ module.exports = (websockets, app, database, flake) => {
                                             websocket.send(JSON.stringify({ event: "tokenChange", newToken: token }));
                                             websocket.terminate();
                                         });
+                                        websockets[token] = websockets[oldToken];
+                                        delete websockets[oldToken];
                                     }
                                     res.send({ token: token });
                                 } else {
