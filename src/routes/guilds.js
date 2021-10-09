15231c78c6d2fd42c2423a486a46dff6a2143f8c
SapphireDisD <villagerlover52@hotmail.com>
2021-10-09 15:33:45 -0300

    Automatic commit

--- src/routes/guilds.js
+++ src/routes/guilds.js
@@ -42,9 +42,8 @@ module.exports = (websockets, app, database, checkLogin, flake) => {
                 }
             database.query(`INSERT INTO guilds (id, name, owner, channels) VALUES($1, $2, $3, $4)`, [guild.id, guild.name, guild.owner, JSON.stringify(guild.channels)], (err, dbRes) => {
                 if (!err) {
-                    database.query(`SELECT guilds FROM users`, (err, dbRes) => {
+                    database.query(`SELECT * FROM users`, (err, dbRes) => {
                         if (!err) {
-                            console.log(dbRes.rows);
                     let guilds = JSON.parse(dbRes.rows.find(x => x.token == req.headers.authorization)[0].guilds);
                     guilds.push(guild.id);
                     database.query(`UPDATE users SET guilds = ${JSON.stringify(guilds)} WHERE id = userId`, (err, dbRes) => {
