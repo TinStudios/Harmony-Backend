3b0ad1eac69b87178ed84b4f5f1d304d323a2029
SapphireDisD <villagerlover52@hotmail.com>
2021-10-09 17:02:57 -0300

    Automatic commit

--- src/routes/account.js
+++ src/routes/account.js
@@ -49,7 +49,6 @@ module.exports = (websockets, app, database, flake) => {
                         const password = await argon2.hash(req.body.password, { type: argon2.argon2id });
                         const token = "Bearer " +  await generateToken({ id: id });
                         const discriminator = generateDiscriminator(dbRes.rows.filter(x => x.username == req.body.username).map(x => x.discriminator) ?? []);
-                        console.log([id, token, req.body.email, password, req.body.username, discriminator, Date.now(), '[]']);
                         database.query(`INSERT INTO users (id, token, email, password, username, discriminator, creation, guilds) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)` [id, token, req.body.email, password, req.body.username, discriminator, Date.now(), '[]'], (err, dbRes) => {
                             if (!err) {
                                 res.status(200).send({ token: token });
