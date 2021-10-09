cbcc8b0fb390b2c43e506827302ac4905b7f4aee
SapphireDisD <villagerlover52@hotmail.com>
2021-10-09 16:58:54 -0300

    Automatic commit

--- src/routes/users.js
+++ src/routes/users.js
@@ -6,7 +6,7 @@ module.exports = (websockets, app, database, checkLogin) => {
                 database.query(`SELECT * FROM users`, async (err, dbRes) => {
                     if (!err) {
                         const user = dbRes.rows.find(x => x.token == req.headers.authorization);
-                database.query(`UPDATE users SET username = $1, discriminator = $2 WHERE id = '${userId}'`, [req.body.username, dbRes.rows.find(x => x.username == req.body.username && x.discriminator == user.discriminator) ? generateDiscriminator(dbRes.rows.filter(x => x.username == req.body.username) : user.discriminator], err => {
+                database.query(`UPDATE users SET username = $1, discriminator = $2 WHERE id = '${userId}'`, [req.body.username, dbRes.rows.find(x => x.username == req.body.username && x.discriminator == user.discriminator) ? generateDiscriminator(dbRes.rows.filter(x => x.username == req.body.username)) : user.discriminator], err => {
                             if (!err) {
                                 res.send(Object.keys(user).reduce((obj, key, index) => key != "token" && key != "password" ? ({ ...obj, [key]: Object.keys(user).map(x => x == "guilds" ? JSON.parse(user[x]) : user[x])[index] }) : null, {}));
                             } else {
