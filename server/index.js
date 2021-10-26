1808f76bd7de69d939c4618d1eadf6d5baf98c2c
08-k <cldoc507@outlook.com>
2021-10-25 21:21:31 -0400

    Updates

---
+++ server/index.js
@@ -0,0 +1,44 @@
+const { Client } = require('pg');
+const { createServer } = require('http');
+const { WebSocket, WebSocketServer } = require('ws');
+
+const app = require('./app');
+const config = require('./utils/config');
+const { createLogger } = require('./utils/logger');
+
+const wss = new WebSocketServer({ noServer: true });
+const ws = new WebSocket(`ws://${config.ws.host}:${config.ws.port}/socket?key=${encodeURIComponent(config.ws.key)}`);
+const database = new Client({
+    user: config.db.user,
+    host: config.db.host,
+    database: config.db.database,
+    password: config.db.password,
+    port: config.db.port,
+});
+
+const logger = createLogger(true);
+const server = createServer(config.env === 'development');
+
+const websockets = new Map();
+
+require('./utils/ws')(wss, websockets, server, database);
+
+require('./routes')(websockets, app, database);
+
+server.listen(config.server.port, async () => {
+    require('./utils/db')(database);
+
+    ws.on('message', data => {
+        const parsed = JSON.parse(data.toString());
+        websockets.get(parsed.for)?.forEach(websocket => {
+            websocket.send(JSON.stringify(parsed.message));
+        });
+    });
+
+    ws.on('close', () => {
+        logger.error('We lost connection to Dot Account. Dot Chat will shutdown.');
+        process.exit(-1);
+    });
+
+  logger.info(`Listening on port ${config.server.port}`);
+});
