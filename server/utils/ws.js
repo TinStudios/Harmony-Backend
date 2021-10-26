de17e1ae5f1fdc394192906d62d37f31ccfd868e
08-k <cldoc507@outlook.com>
2021-10-25 21:22:51 -0400

    Move files

---
+++ server/utils/ws.js
@@ -0,0 +1,23 @@
+const config = require('./config');
+
+module.exports = (wss, websockets, server, database) => {
+    server.on('upgrade', (request, socket, head) => {
+        const pathname = request.url.split('?')[0];
+        const token = decodeURIComponent(request.url.split('token=')[request.url.split('token=').length - 1]);
+        require('needle').get(`${config.ws.host}:${config.ws.port}/users/@me`, {
+            headers: {
+                'Authorization': token
+            }
+        }, function (err, resp) {
+            if (pathname === '/socket' && !err && resp.statusCode == 200) {
+                wss.handleUpgrade(request, socket, head, (ws) => {
+                    var websocketForThis = websockets.get(resp.body.id) ?? [];
+                    websocketForThis.push(ws);
+                    websockets.set(resp.body.id, websocketForThis);
+                });
+            } else {
+                socket.destroy();
+            }
+        });
+    });
+}
